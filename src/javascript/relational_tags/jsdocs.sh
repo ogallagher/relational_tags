ae9af44bb042738becbf3a0bdc3ad3dc1c9ef099
ogallagher <owengall@icloud.com>
2023-12-03 09:38:06 -0500

    js npm exec in jsdocs.sh

--- src/javascript/relational_tags/jsdocs.sh
+++ src/javascript/relational_tags/jsdocs.sh
@@ -4,7 +4,7 @@ out_dir="../../../docs/jsdocs"
 pkg_version=$(sed -e 's/^"//' -e 's/"$//' <<< $(npm pkg get version))
 
 # must be run from the js project root where package.json is found
-./node_modules/jsdoc/jsdoc.js \
+npm exec -- jsdoc \
   --readme ../../../readme.md \
   --destination $out_dir \
   --package ./package.json \
